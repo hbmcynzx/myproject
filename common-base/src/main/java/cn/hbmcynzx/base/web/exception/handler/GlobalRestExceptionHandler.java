a6e37122e8f78924b581981d69eeed3fe590c53d
hbmcynzx <857918356@qq.com>
2020-11-10 14:31:20 +0800

    mvc统一异常处理修改

--- common-base/src/main/java/cn/hbmcynzx/base/web/exception/handler/GlobalRestExceptionHandler.java
+++ common-base/src/main/java/cn/hbmcynzx/base/web/exception/handler/GlobalRestExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 全局ajax请求异常处理
  * @author hbmcynzx
@@ -25,29 +27,27 @@ public class GlobalRestExceptionHandler {
      * @return
      */
     @ExceptionHandler(Exception.class)
-    public ResultMsg exceptionHandler(Exception e) {
-        log.error("请求异常：" + e.getMessage(), e);
-        return ResultMsg.error(errorMsg);
+    public ResultMsg exceptionHandler(HttpServletRequest request, Exception e) {
+        ResultMsg msg = null;
+        log.error("请求异常：请求路径：{}，异常信息：{}", request.getRequestURI(), e.getMessage());
+        log.error("请求异常：", e);
+        if(e instanceof BusinessException) {
+            //业务异常，返回业务异常信息
+            msg = ResultMsg.error(e.getMessage());
+        } else if(e instanceof MissingServletRequestParameterException) {
+            //http request参数缺失异常
+            msg = handleMissingServletRequestParameterException((MissingServletRequestParameterException) e);
+        } else {
+            msg = ResultMsg.error(errorMsg);
+        }
+        return msg;
     }
 
     /**
-     * controller抛出BusinessException异常时，返回json数据
-     * @param e
+     * 处理参数缺失异常
      * @return
      */
-    @ExceptionHandler(BusinessException.class)
-    public ResultMsg businessExceptionHandler(BusinessException e) {
-        log.error("请求异常：" + e.getMessage(), e);
-        return ResultMsg.error(e.getMessage());
-    }
-
-    /**
-     * controller抛出MissingServletRequestParameterException异常时，返回json数据
-     * @param e
-     * @return
-     */
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResultMsg businessExceptionHandler(MissingServletRequestParameterException e) {
+    private ResultMsg handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
         return ResultMsg.error(String.format("缺失参数[%s:%s]", e.getParameterType(), e.getParameterName()));
     }
 
